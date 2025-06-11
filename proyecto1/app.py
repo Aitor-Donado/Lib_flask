60a605178693c34f0f0845f6d5fc08223a8970dc
Aitor Donado <aitor.donado@gmail.com>
2025-06-11 14:01:58 +0200

    Agregada recepción de datos del formulario

--- proyecto1/app.py
+++ proyecto1/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,9 +10,20 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        # Aquí puedes manejar el envío del formulario
+        # Capturar los datos del formulario
+        form_data = {
+            'name': request.form.get('name'),
+            'email': request.form.get('email'),
+            'message': request.form.get('message')
+        }
+        print(form_data)
+        return render_template("contact_enviado.html", form_data=form_data)
+    else:
+        return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
