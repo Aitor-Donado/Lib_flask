dc08401196c6e8f2a024fa9c0ad323783c93db60
Aitor Donado <aitor.donado@gmail.com>
2025-06-12 10:39:35 +0200

    Añadido form

--- mi_pagina_personalizada/app.py
+++ mi_pagina_personalizada/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta'
@@ -8,8 +9,13 @@ app.secret_key = 'tu_clave_secreta'
 def inicio():
     return render_template("inicio.html", color_fondo="lightblue")
 
-@app.route('/configurar')
+@app.route('/configurar', methods=['GET', 'POST'])
 def configurar():
+    if request.method == 'POST':
+        # Aquí puedes agregar la lógica para guardar los cambios
+        print("request.form: ", request.form)
+        return redirect(url_for('inicio'))
+    # Aquí puedes agregar la lógica para obtener los valores actuales de la configuración
     return render_template("configurar.html")
 
 if __name__ == '__main__':
